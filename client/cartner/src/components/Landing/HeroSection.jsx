1ed8087f4332326c328cbb594ed5353eb2043214
BMAsekunowo <bm.asekunowo@gmail.com>
2025-07-24 22:30:44 +0100

    cartner-hero.png fix

--- client/cartner/src/components/Landing/HeroSection.jsx
+++ client/cartner/src/components/Landing/HeroSection.jsx
@@ -3,7 +3,7 @@ import { toast } from "react-toastify";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../styles/HeroSection.css";
-import HeroImage from "../../assets/landing/cartner-hero.png";
+import HeroImage from "../../assets/landing/cartner-hero.PNG";
 import Button from "../Reusables/Button";
 import { createSession } from "../../services/SessionService";
 
